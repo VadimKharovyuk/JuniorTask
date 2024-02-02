4feb370c25e36418ce153381aa86d00efcc521cc
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-02-03 05:23:50 +0530

    need Docker

--- src/main/java/com/example/juniortask/JuniorTaskApplication.java
+++ src/main/java/com/example/juniortask/JuniorTaskApplication.java
@@ -9,6 +9,7 @@ public class JuniorTaskApplication {
     public static void main(String[] args) {
         SpringApplication.run(JuniorTaskApplication.class, args);
         //    https://www.youtube.com/watch?v=8W5CeSGcAU0&list=LL&index=4&t=3042s
+        //02 46
     }
 
 }
