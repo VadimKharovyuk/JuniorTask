c485decdeabb2ee5b58d2bd2fe30e0eb9559e473
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-02-03 03:02:41 +0530

    Initial commit

---
+++ src/main/java/com/example/juniortask/JuniorTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.juniortask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuniorTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuniorTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/juniortask/JuniorTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.juniortask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuniorTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
